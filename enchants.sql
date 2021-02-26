f27587a2eed104d43ccd07f6632996f2a7331f39
Ferronn <ferronn@ferronn.dev>
2021-02-25 21:02:41 -0600

    better support weapon enchants

--- enchants.sql
+++ enchants.sql
@@ -5,7 +5,8 @@ FROM (
     SELECT
         CAST(e.spellid AS INT64) AS spell,
         CAST(e.effectmiscvalue_0_ AS INT64) AS effect,
-        CAST(q.equippediteminvtypes AS INT64) AS invmask
+        CAST(q.equippediteminvtypes AS INT64) AS invmask,
+        (q.equippeditemclass = '2') AS for_weapon
     FROM
         spelleffect e,
         spellequippeditems q,
@@ -20,6 +21,7 @@ FROM (
         n.name_lang not like "QA%"
 ), UNNEST(GENERATE_ARRAY(1, 19)) slot
 WHERE
-    invmask & (1 << slot) > 0
+    invmask & (1 << slot) > 0 OR
+    (slot in (16, 17, 18) and for_weapon)
 GROUP BY slot
 ORDER BY slot;
